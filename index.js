d85f0301ccf4574195974c5c566686fb5522634c
Tangila Khatun <ktangila6@gmail.com>
2025-12-07 19:43:07 +0600

    Setup MongoDB connection in backend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors =require('cors')
 const app = express();
 require('dotenv').config()
@@ -7,6 +8,30 @@ const port =process.env.PORT || 3000
 // middleware 
 app.use(express.json());
 app.use(cors())
+
+// ZOPEW8wI8GrK91MJ 
+
+const uri = `mongodb+srv://${process.env.MANAGEMENT_DB}:${process.env.MANAGEMENT_PASSWORD}@cluster0.ri8wtve.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+let db;
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db("assetverse");
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err);
+  }
+}
+connectDB();
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
